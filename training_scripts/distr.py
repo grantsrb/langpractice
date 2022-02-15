4e8a0da31e51ed768990b6d247646fbf80bc9161
Satchel Grant <grantsrb@stanford.edu>
2022-02-15 11:37:38 -0800

    enable multiple split keys

--- training_scripts/distr.py
+++ training_scripts/distr.py
@@ -67,24 +67,33 @@ def split_ranges(meta):
     save_path[-1] = load_json(meta["hyperparams"])["exp_name"]
     save_path = "/".join(save_path)
 
-    splt_key = meta["split_key"]
-    keys = list(filter(lambda x: x!=splt_key, ranges.keys()))
+    if "split_keys" in meta:
+        splt_keys = set(meta["split_keys"])
+    elif "split_key" in meta:
+        splt_keys = [meta["split_key"]]
+    else:
+        assert False, "split_key or split_keys must be in metaparameters"
+    keys = list(filter(lambda x: x not in splt_keys, ranges.keys()))
     no_splt = {k:ranges[k] for k in keys}
-
     devices = meta["devices"]
 
+    # Divide up hyperranges equally amongst GPUs
     rng_paths = []
-    for device,val in zip(devices, ranges[splt_key]):
-        rngs = {splt_key: [val], **no_splt}
+    range_dict = {i:{**no_splt} for i in devices}
+    for splt_key in splt_keys:
+        splt_len = len(ranges[splt_key])
+        n_vals = splt_len//len(devices) # Number of split_key values per gpu
+        if splt_len % len(devices) != 0:
+            print("WARNING: SEARCH DIVIDED UNEQUALLY!!!")
+        for i,device in enumerate(devices):
+            vals = ranges[splt_key][i*n_vals:(i+1)*n_vals]
+            range_dict[device] = {splt_key: vals, **range_dict[device]}
+
+    # Save hyperranges to json files
+    for device in devices:
         path = save_path+"{}.json".format(device)
         rng_paths.append(path)
-        save_json(rngs, path)
-    if len(devices) < len(ranges[splt_key]):
-        print("NOT ENOUGH DEVICES ARGUED FOR SPLIT KEY!!!")
-        s = splt_key + " values missing: "
-        for rng in ranges[splt_key][len(devices):]:
-            s += str(rng) + ", "
-        print(s[:-2])
+        save_json(range_dict[device], path)
     return rng_paths
 
 if __name__ == "__main__":
